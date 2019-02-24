8c1f440fa4eeb4104a97cf6aee8b81921130a518
yoshitakanakajima <buyu.naka2@gmail.com>
2019-02-25 00:02:11 +0900

    topページと共有部分HTML実装

---
+++ js/layout.js
@@ -0,0 +1,9 @@
+/**
+ * 共通部読み込み
+ */
+$(function(){
+  $("#header").load("component/header.html .header");
+  $("#footer").load("component/footer.html .footer");
+  $("#introduction").load("component/introduction.html .introduction");
+  $("#download").load("component/document.html .download");
+})
