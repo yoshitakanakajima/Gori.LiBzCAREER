593770e2175b804a35dc8494fa097242a61a946a
yoshitakanakajima <buyu.naka2@gmail.com>
2019-02-26 06:03:39 +0900

    導入事例CSS修正

--- js/layout.js
+++ js/layout.js
@@ -2,8 +2,17 @@
  * 共通部読み込み
  */
 $(function(){
-  $("#header").load("component/header.html .header");
+  $("#header").load("component/header.html .header", function() {
+	  var url = location.href;
+	  if (url.indexOf('introduction_list') != 0) {
+		  $("#menu-1").addClass("menu-on");
+	  } else {
+		  $("#menu-1").removeClass("menu-on");
+	  }
+  });
   $("#footer").load("component/footer.html .footer");
   $("#introduction").load("component/introduction.html .introduction");
   $("#download").load("component/document.html .download");
+  $("#consultation").load("component/consultation.html .consultation");
+  $("#related-article").load("component/related_article.html .related-article");
 })
