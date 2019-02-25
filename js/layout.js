8bfa6f47fea6fc08c40fe43e6e9d296e572f2d39
yoshitakanakajima <buyu.naka2@gmail.com>
2019-02-26 06:44:40 +0900

    資料請求CSS修正

--- js/layout.js
+++ js/layout.js
@@ -4,10 +4,13 @@
 $(function(){
   $("#header").load("component/header.html .header", function() {
 	  var url = location.href;
-	  if (url.indexOf('introduction_list') != 0) {
+	  if (url.indexOf('introduction') >= 0) {
 		  $("#menu-1").addClass("menu-on");
+	  } else if (url.indexOf('document.html') >= 0) {
+		  $("#menu-2").addClass("menu-on");
 	  } else {
 		  $("#menu-1").removeClass("menu-on");
+		  $("#menu-2").removeClass("menu-on");
 	  }
   });
   $("#footer").load("component/footer.html .footer");
