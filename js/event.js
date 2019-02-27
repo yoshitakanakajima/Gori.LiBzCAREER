48e2d2e59e0f25f91f674889a4da4711ac287a67
yoshitakanakajima <buyu.naka2@gmail.com>
2019-02-27 20:09:36 +0900

    導入事例のSP実装

--- js/event.js
+++ js/event.js
@@ -27,3 +27,11 @@ $(document).on('mouseenter','#menu-3', function() {
 $(document).on('mouseout','#menu-3', function() {
     $('#menu-3').css('background', '');
 });
+
+
+$(document).on('click touchstart','#menu-intro', function() {
+	window.location.href = 'introduction_list.html';
+});
+$(document).on('click touchstart','#menu-doc', function() {
+	window.location.href = 'document_list.html';
+});
